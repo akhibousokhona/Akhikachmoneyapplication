3457e7064a243a09dddd3545a49beb589edbe9ad
akhibousokhona <akhibou1sokhona@gmail.com>
2022-02-22 15:04:02 +0100

    second commit

--- connexion/connexion/settings.py
+++ connexion/connexion/settings.py
@@ -15,7 +15,7 @@ import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
